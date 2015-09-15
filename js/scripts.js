908bf88931555868ed9d2950a71efbcbaeeb8a1d
Kevin Tokheim & jordan navratil <student@epicodus.com>
2015-09-15 16:42:49 -0700

    added more functionality and passing tests.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,21 @@ function Ticket(movieTitle, movieTime, age, price){
 
 var firstRelease = ["Humpty Dumpty's Revenge", "Dances with Avatar", "Braveheart: the Musical", "Lethal Weapon:the Musical","Adventures with Jaba the Hut"];
 
-
+//Tests are passing, but may not be working like we intend them to
+//Do Specific-to-General-
 Ticket.prototype.ticketPrice = function() {
   for(var i = 0; 0 <= firstRelease.length; i++) {
     if (this.movieTitle == firstRelease[i]) {
       this.price = 15;
-    } else if (this.movieTime < "5:00pm") {
+    } else if (this.movieTime < "5:00pm") { // "5:00pm" is not working
+      this.price = 10;
+    } else if (this.age < 13 || this.age > 60) {
+      this.price = 10;
+    } else if (this.movieTitle == firstRelease[i] && this.movieTime < "5:00pm") {
+      this.price = 10;
+    } else if (this.movieTitle == firstRelease[i] && this.age < 13){
+      this.price = 10;
+    } else if (this.movieTitle == firstRelease[i] && this.age > 60){
       this.price = 10;
     }
     return this.price;
--- specs/specs.js
+++ specs/specs.js
@@ -1,19 +1,39 @@
 describe("Ticket", function(){
   it("creates a new ticket price with the given specifications", function(){
-    var testTicket = new Ticket("Terminator", "7:00pm", "15", 10);
+    var testTicket = new Ticket("Terminator", "7:00pm", 15, 10);
     expect(testTicket.movieTitle).to.equal("Terminator");
     expect(testTicket.movieTime).to.equal("7:00pm");
-    expect(testTicket.age).to.equal("15");
+    expect(testTicket.age).to.equal(15);
     expect(testTicket.price).to.equal(10);
   });
 
   it("returns the price of a first-release movie ticket", function(){
-    var testTicket = new Ticket("Humpty Dumpty's Revenge", "7:00pm", "15", 10);
+    var testTicket = new Ticket("Humpty Dumpty's Revenge", "7:00pm", 15, 10);
     expect(testTicket.ticketPrice()).to.equal(15);
   });
 
   it("returns the price of a movie before 5:00pm", function(){
-    var testTicket = new Ticket("Terminator", "4:30pm", "15", 5);
+    var testTicket = new Ticket("Terminator", "4:30pm", 15, 5);
+    expect(testTicket.ticketPrice()).to.equal(10);
+  });
+
+  it("returns the price of a child or senior ticket", function() {
+    var testTicket = new Ticket("Predator", "8:00", 9, 5);
+    expect(testTicket.ticketPrice()).to.equal(10);
+  });
+
+  it("returns the price of a ticket for a first release movie before 5:00pm", function(){
+    var testTicket = new Ticket("Braveheart: the Musical", "4:00pm", "9", 4);
+    expect(testTicket.ticketPrice()).to.equal(10);
+  });
+
+  it("returns the price of a ticket for a first release movie for a child", function(){
+    var testTicket = new Ticket("Braveheart: the Musical", "5:00pm", 12, 4);
+    expect(testTicket.ticketPrice()).to.equal(10);
+  });
+
+  it("returns the price of a ticket for a first release movie for a senior", function(){
+    var testTicket = new Ticket("Braveheart: the Musical", "8:00pm", 65, 4);
     expect(testTicket.ticketPrice()).to.equal(10);
   });
 });
